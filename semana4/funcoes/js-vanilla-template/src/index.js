d06e9c5bb9498d621f7782676aff726309f9c65c
mayaramaaraujo <mayar@LAPTOP-4P9D6LRT.(none)>
2020-08-03 15:46:23 -0400

    Resolução dos exercícios 1 e 2 de interpretação de código

---
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -0,0 +1,102 @@
+// EXEMPLO 1 
+
+// function dizOi() {
+//     console.log("oi")
+// }
+
+// dizOi()
+
+
+// EXEMPLO 2
+
+// ------------------------------------------- EXEMPLO DE FUNCAO NOMEADA -----------------
+
+// const resultado1 = somaDoisNumeros(5, 6)
+// const resultado2 = somaDoisNumeros(10, 20)
+
+// console.log(resultado1)
+// console.log(resultado2)
+
+// function somaDoisNumeros(numero1, numero2) {
+//     const soma = numero1 + numero2
+//     return soma
+// }
+
+
+
+
+// ------------------------------------------- EXEMPLO DE FUNCAO NÃO NOMEADA -----------------
+
+
+// const somaDoisNumeros = function(numero1, numero2) {
+//     const soma = numero1 + numero2
+//     return soma
+// }
+    
+// const resultado1 = somaDoisNumeros(5, 6)
+// const resultado2 = somaDoisNumeros(10, 20)
+
+// console.log(resultado1)
+// console.log(resultado2)
+
+
+
+// ------------------------------------------- EXEMPLO DE ARROW FUNCTION (FUNCAO FLECHA) -----------------
+
+
+// const somaDoisNumeros = (numero1, numero2) => {
+//     const soma = numero1 + numero2
+//     return soma
+// }
+
+
+// const resultado1 = somaDoisNumeros(5, 6)
+// const resultado2 = somaDoisNumeros(10, 20)
+
+// console.log(resultado1)
+// console.log(resultado2)
+
+
+
+
+// ------------------------------------------- EXERCÍCIO 1 -----------------
+
+// function verificaArray(arrayDeParametro) {
+//     for (let elemento of arrayDeParametro) {
+//         console.log(elemento)
+//     }
+
+//     return arrayDeParametro.length 
+// }
+
+
+// ------------------------------------------- EXERCÍCIO 2 -----------------
+
+
+// const verificaTamanhoDoArray = (arrayDeParametro) => {
+//     for (let elemento of arrayDeParametro) {
+//         console.log(elemento)
+//     }
+
+//     return arrayDeParametro.length
+// }
+
+// const profs = ["chijo", "paulinha", "sot", "caio", "severo"]
+
+// const tamanhoDoArray = verificaTamanhoDoArray(profs)
+
+//~~~~~~~~~~~~~~~~ EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO ~~~~~~~~~~~~~~~//
+
+//1.
+    /* 
+        a. 10 e 50
+        b. A função retornaria os valores sem mostrar no console.
+    */
+//2.
+    /*
+        a. Apareceria os dois primeiros nomes do arrayDeNomes, no caso, Darvas e Goli.
+        b. Apareceria no lugar de Darvas e Goli, Amanda e Caio.
+    */
+//3. 
+    /*
+        a.
