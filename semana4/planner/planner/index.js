075b39fdbe04db31a24ba0e1a48191e7cca42429
mayaramaaraujo <mayar@LAPTOP-4P9D6LRT.(none)>
2020-08-04 12:13:13 -0400

    Resolução de alguns dos desafios

--- semana4/planner/planner/index.js
+++ semana4/planner/planner/index.js
@@ -5,11 +5,28 @@ function adicionarTarefa(){
     let diasSemanas = document.getElementById("dias-semana")
     let diaDaSemana = diasSemanas.value
 
+    let horarios = document.getElementById("horarios")
+    let horarioValor = horarios.value
+
     if(tarefaValor !== ""){ 
         let tarefaDia = document.getElementById(`${diaDaSemana}`)
-        tarefaDia.innerHTML += `<p>${tarefaValor}</p>`
+        tarefaDia.innerHTML += `<p onclick="riscarTarefa()"><strong>${horarioValor}</strong> <br> ${tarefaValor}</p>`
         tarefa.value = ""             
     } else {
         alert("Digite uma tarefa válida!")
     }   
 }
+
+function riscarTarefa(){
+ 
+}
+
+function limparTodasAsTarefas() {
+    let arraySemana = ["domingo", "segunda", "terca", "quarta", "quinta", "sexta", "sabado", "domingo"]
+
+    for(let elemento of arraySemana){
+        let tarefaDia = document.getElementById(`${elemento}`)
+        tarefaDia.innerHTML = ""
+    }
+}
+
