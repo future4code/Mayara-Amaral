c604c637260d6e67fe3b4d2765a064e0e35e325f
mayaramaaraujo <mayar@LAPTOP-4P9D6LRT.(none)>
2020-08-06 06:51:44 -0400

    adicionei a funcao que adiciona o post

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,38 +1,33 @@
-let titulo = document.getElementById("titulo-post")
-let autor = document.getElementById("autor-post")
-let conteudo = document.getElementById("conteudo-post")
-let posts = document.getElementById("container-de-posts")
-let container = []
+const titulo = document.getElementById("titulo-post")
+const autor = document.getElementById("autor-post")
+const conteudo = document.getElementById("conteudo-post")
+const container = document.getElementById("container-de-posts")
+let arrayDePosts = []
+let post = {
+    titulo:"",
+    autor:"",
+    conteudo:""
+}
 
-function capturarInput(){   
-    post.titulo = titulo.value
-    post.autor = autor.value
-    post.conteudo = conteudo.value    
-    container.push(post)
-    console.log(container)
+
+function adicionarPost() { 
+    let tituloDigitado = titulo.value
+    let autorDigitado = autor.value
+    let conteudoDigitado = conteudo.value  
+
+    post.titulo = tituloDigitado
+    post.autor = autorDigitado
+    post.conteudo = conteudoDigitado
+    
+    container.innerHTML += `<p><h2> ${post.titulo} </h2><br> ${post.autor} <br> ${post.conteudo} </p>`  
+    
+
     titulo.value = ""
     autor.value = ""
     conteudo.value = ""
+
+    arrayDePosts.push(post)       
 }
-
-const post = {
-    titulo: "",
-    autor: "",
-    conteudo: "",
-    adicionar: capturarInput
-}
-
-
-
-console.log(post)
-
-
-
-
-
-
-
-
 
 
 
