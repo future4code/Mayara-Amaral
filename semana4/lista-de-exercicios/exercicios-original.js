5b1a72a54e2446b66b2a4d64b231de53aba70085
mayaramaaraujo <mayar@LAPTOP-4P9D6LRT.(none)>
2020-08-07 13:47:27 -0400

    resolução exercicio 4 interpretação de código

--- semana4/lista-de-exercicios/exercicios-original.js
+++ semana4/lista-de-exercicios/exercicios-original.js
@@ -9,5 +9,11 @@
 
 2.
     O código declara uma função que tem como objetivo calcular o valor após o investimento, recebendo como parâmetro o tipo de investimento e o valor. Por meio do switch ele calcula o montante de acordo com o tipo de investimento. Caso não seja nenhum, ou diferente das opções, a função retorna um alerta para o usuário. Após isso, a chamada da função é feita dentro de uma constante, armazenando o retorno dela.
+
 3.
+    No código, há três constantes que armazenam arrays. O loop for, faz com que para cada elemento (numero) do array (numeros), tenha uma verificação se o número é par. Caso seja par, ele acrescenta no array1. Caso não seja, acrescenta no array2. Ou seja, separa os números impares dos pares. Depois é impresso no console a "Quantidade total de números 12" e a quantidade de números pares e impares.
+
+4.
+    O código verifica se os números do array numeros é positivo ou negativo. Caso seja negativo, ele atribui a variável numero1 o número presente no indice atual do array. Caso seja positivo ele atribui a variável numero2 o valor do numero presente no atual indice do array. E depois imprime no console, o número1 e número2.
+
 
