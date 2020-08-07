8796568b0cd7dbccb6bfb06fe266146df03a7a69
mayaramaaraujo <mayar@LAPTOP-4P9D6LRT.(none)>
2020-08-07 13:36:29 -0400

    resolução exercício de interpretação de código numero 2.

--- semana4/lista-de-exercicios/exercicios-original.js
+++ semana4/lista-de-exercicios/exercicios-original.js
@@ -2,4 +2,12 @@
 
 //LISTA DE EXERCÍCIOS 
 
-/* ~~~~~~~~~~~~ EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO ~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~ EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO ~~~~~~~~~~~ 
+
+1.
+    No código é declarada uma função que recebe como parâmetro o valor em dólar e recebe do usuário pelo prompt a cotação do dólar, retornando o valor em reais armazenado na constante meuDinheiro, e por fim, imprime no console.
+
+2.
+    O código declara uma função que tem como objetivo calcular o valor após o investimento, recebendo como parâmetro o tipo de investimento e o valor. Por meio do switch ele calcula o montante de acordo com o tipo de investimento. Caso não seja nenhum, ou diferente das opções, a função retorna um alerta para o usuário. Após isso, a chamada da função é feita dentro de uma constante, armazenando o retorno dela.
+3.
+
