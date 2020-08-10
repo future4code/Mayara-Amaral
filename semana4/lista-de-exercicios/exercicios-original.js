96e1ff13056f0f232a416f688d125731b81308ae
mayaramaaraujo <mayar@LAPTOP-4P9D6LRT.(none)>
2020-08-10 15:48:43 -0400

    resolução exercicio 4 funcoes de array

--- semana4/lista-de-exercicios/exercicios-original.js
+++ semana4/lista-de-exercicios/exercicios-original.js
@@ -264,7 +264,35 @@ ficarAnonimo(pessoa)
 
 //EXERCÍCIO 3.  
 
-        
+    const pessoas = [
+    	{ nome: "Paula", idade: 12, altura: 1.8},
+    	{ nome: "João", idade: 20, altura: 1.3},
+    	{ nome: "Pedro", idade: 15, altura: 1.9},
+    	{ nome: "Luciano", idade: 22, altura: 1.8},
+    	{ nome: "Artur", idade: 10, altura: 1.2},
+    	{ nome: "Soter", idade: 70, altura: 1.9}
+    ]
+
+    //a.
+
+        const pessoasQuePodemBrincar = pessoas.filter((elemento, index, array) => {
+            if(elemento.idade > 14 && elemento.idade < 60 && elemento.altura > 1.5){
+                return true
+            } 
+            return false
+        })
+
+        console.log(pessoasQuePodemBrincar)
+
+    //b. 
+        const pessoasQueNaoPodemBrincar = pessoas.filter((elemento, index, array) => {
+            if(elemento.idade > 14 && elemento.idade < 60 && elemento.altura > 1.5){
+                return false
+            } 
+            return true
+        })
+
+        console.log(pessoasQueNaoPodemBrincar)
 
         
 
