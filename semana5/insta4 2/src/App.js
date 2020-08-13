0ea8beb2adf0d86657133770fb04a9aadbb31222
mayaramaaraujo <mayar@LAPTOP-4P9D6LRT.(none)>
2020-08-13 16:31:07 -0400

    styled components

--- semana5/insta4 2/src/App.js
+++ semana5/insta4 2/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Post from './components/Post/Post';
-import styled from './styled-components'
+import styled from ''
 
 const AdicionandoPost = styled.div `
   display: flex;
