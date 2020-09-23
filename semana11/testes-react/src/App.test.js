c1f7407d7d72c90a638c25231054c7f600b5d2df
mayaramaaraujo <mayar@LAPTOP-4P9D6LRT.(none)>
2020-09-23 15:17:58 -0400

    teste

--- semana11/testes-react/src/App.test.js
+++ semana11/testes-react/src/App.test.js
@@ -1,45 +1,59 @@
 import React from "react";
 import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent, getByPlaceholderText, getByTestId } from "@testing-library/react";
+import { render, fireEvent} from "@testing-library/react";
 import App from "./App";
 import Post from './components/Post'
 
 test("Verifica criação de post", async () => {
-    const { getByText } = render(<App />); 
+    const { getByPlaceholderText, getByText } = render(<App />); 
 
-    const addPost = getByText(/Adicionar/i)
+    const input = getByPlaceholderText("Novo post")
 
-    fireEvent.click(addPost)
+    fireEvent.change(input,  {target:  { value: "teste post"}})
 
-    expect(getByText(/Curtir/i)).toHaveTextContent("Curtir")
+    expect(input).toHaveValue("teste post")
+
+    const button = getByText("Adicionar")
+
+    fireEvent.click(button)
+
+    const post = getByText("teste post")
+
+    expect(post).toBeInTheDocument()
+
+    const likeButton = getByText("Curtir")
+
+    fireEvent.click(likeButton)
+
+    expect(likeButton).toHaveTextContent("Descurtir")
 })
 
-test("Verifica curtida", async () => {
-
-    const { getByText } = render(<App />)
-
-    const addPost = getByText(/Adicionar/i)
-
-    fireEvent.click(addPost)
-
-    const curtir = getByText(/Curtir/i)
-
-    fireEvent.click(curtir)
-
-    expect(getByText(/Descurtir/i)).toHaveTextContent("Descurtir")
-})
-
-test("Verifica se post foi apagado", async () => {
-    const { getByText } = render (<App />);
-
-    const addPost = getByText(/Adicionar/i)
-
-    fireEvent.click(addPost)
-
-    const apagar = getByText(/Apagar/i)
-
-    fireEvent.click(apagar)  
-    
-    expect().toHaveTextContent(null)
-
-})
+//test("Verifica curtida", async () => {
+//
+//    const { getByText } = render(<App />)
+//
+//    const addPost = getByText(/Adicionar/i)
+//
+//    fireEvent.click(addPost)
+//
+//    const curtir = getByText(/Curtir/i)
+//
+//    fireEvent.click(curtir)
+//
+//    expect(getByText(/Descurtir/i)).toHaveTextContent("Descurtir")
+//})
+//
+//test("Verifica se post foi apagado", async () => {
+//    const { getByText } = render (<App />);
+//
+//    const addPost = getByText(/Adicionar/i)
+//
+//    fireEvent.click(addPost)
+//
+//    const apagar = getByText(/Apagar/i)
+//
+//    fireEvent.click(apagar)  
+//    
+//    expect().toHaveTextContent(null)
+//
+//})
