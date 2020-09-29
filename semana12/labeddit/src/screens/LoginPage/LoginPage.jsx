caad3f8370a36e9e470bbbf4060f51f8c2f8c53d
mayaramaaraujo <mayar@LAPTOP-4P9D6LRT.(none)>
2020-09-29 05:35:35 -0400

    form

--- semana12/labeddit/src/screens/LoginPage/LoginPage.jsx
+++ semana12/labeddit/src/screens/LoginPage/LoginPage.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useHistory } from 'react-router-dom';
 
 export function LoginPage() {
     const history = useHistory()
+    const [form, setForm] = useState({ email: '', password: ''})
+
+    const handleInputEvent = (event) => {
+        const { name, value } = event.target
+        setForm({...form, [name]: value})
+    }
+
     return (
         <div>
             LoginPage
+            <input 
+                name="email"
+                value={form.email}
+                onChange={handleInputEvent}
+                type="email"
+            />
+            <input 
+                name="password"
+                value={form.password}
+                onChange={handleInputEvent}
+                type="password"
+            />     
+
         </div>
     )
 }
