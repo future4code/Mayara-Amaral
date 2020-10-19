5d7d9a4f3ef8a8939522d97244b113b6999bf234
Mayara Araujo <mayaramaaraujo98@gmail.com>
2020-10-19 15:39:07 -0400

    desafio de persistir dados ok

--- semana13/aula40/exercicio3/index.js
+++ semana13/aula40/exercicio3/index.js
@@ -2,8 +2,21 @@
 
 const task = process.argv[2]
 
-const arrayTasks = ["Lavar louça"];
+const arrayTasks = [];
 
 arrayTasks.push(task)
 
-console.log(arrayTasks)
+const fs = require("fs");
+
+fs.appendFile("arrayTasks.txt", `,${task}`, function (err) { 
+    if(err) throw err
+});
+
+let tarefas = "";
+
+fs.readFile("arrayTasks.txt", "utf-8", function(err, data) {
+    tarefas = data
+    let arr = tarefas.split(",")
+
+    console.log(arr)
+})
